d00fecc6e90f7c2e83d83791b06a33d72aaf1ec6
chenlin <lindits163.com>
2022-03-29 17:18:35 +0800

    测试push m1

--- LLPushTools/Public/LLPushManager.swift
+++ LLPushTools/Public/LLPushManager.swift
@@ -10,7 +10,7 @@ import UIKit
 public class LLPushManager: NSObject {
     
     public class func sayHello() {
-        print("hello work")
+        print("hello work test")
     }
     
     public class func run() {
